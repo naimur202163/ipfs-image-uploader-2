25a222068eafce3a14bd08b5af9e6236b1962783
Your Name~ <naiumur202163@gmail.com>
2022-03-09 13:01:09 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { create } from 'ipfs-http-client'
+import React, { useState } from 'react'
+import './App.css'
+const client = create('https://ipfs.infura.io:5001/api/v0')
 function App() {
+
+  const [fileUrl, updateFileUrl] = useState(``)
+
+  console.log(fileUrl)
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      updateFileUrl(url);
+      
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'> <h1>IPFS Example</h1>
+    <input
+      type="file"
+      onChange={onChange}
+    />
+    {
+      fileUrl && (
+        <img src={fileUrl} width="600px" alt='' />
+      )
+    }</div>
+  )
 }
 
-export default App;
+export default App
