76d37b04528a3d9bc4a6b28a91e74aeb68cad47a
Your Name~ <naiumur202163@gmail.com>
2022-03-09 14:48:07 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { create } from 'ipfs-http-client';
 import React, { useState } from "react";
 import './App.css';
+
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 function App() {
   const [loginData,setLoginData]=useState({})
+  const [fileUrl, updateFileUrl] = useState(``)
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      updateFileUrl(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
 
        
     const handleOnBlur=(e)=>{
@@ -26,10 +41,24 @@ function App() {
         <input name="emai" type="email" placeholder="enter  you email" onBlur={handleOnBlur}/>
         <br/>
         <br/>
-        <input name="number" type="number" placeholder="enter you number    "  onBlur={handleOnBlur}/>
+        <input name="number" type="number" placeholder="enter you number"  onBlur={handleOnBlur}/>
 
         <button type="submit">Submit</button>
       </form>
+      <br/>
+      <br/>
+      <br/>
+      <h1>IPFS Example</h1>
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      {
+        fileUrl && (
+          <img src={fileUrl} width="600px" />
+        )
+      }
+     
     </div>
   );
 }
